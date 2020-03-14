ad6255bba6dafd6f9d4e7d127c25f7a3d65503c5
John Untivero <jpuntivero@gmail.com>
2020-03-14 05:09:25 -0500

    ThermoActive updated  new ThermoHouse constructor

--- src/main/java/edu/occc/ecp/ThermoActive.java
+++ src/main/java/edu/occc/ecp/ThermoActive.java
@@ -3,8 +3,8 @@ package edu.occc.ecp;
 public class ThermoActive {
 
     public static void main(String[] args) {
-        ThermoHouse t1 = new ThermoHouse(75, 81, 63, 2);
-        ThermoHouse t2 = new ThermoHouse(58, 21, 53, 1);
+        ThermoHouse t1 = new ThermoHouse(75);
+        ThermoHouse t2 = new ThermoHouse(58);
 
         System.out.println("Room 1 is " + t1.toString());
         System.out.println("Room 2 is " + t2.toString());
